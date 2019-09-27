43cf6312ab860b0d2789cdfe1942fda824a8b232
xavieroldan <xavieroldan@gmail.com>
2019-09-27 14:36:32 +0200

    Play select player done> before unify js file

--- js/new_player.js
+++ js/new_player.js
@@ -1,5 +1,5 @@
-var idPlayer="";
 var urlServer="http://localhost:8080/";
+var myPlayer = { name : null, idPlayer : null};
 
 //Create a new player
 //✧*｡٩(ˊᗜˋ*)و✧*｡   
@@ -22,8 +22,8 @@ function newplayer(){
         success: function(data)
                     {                        
                         //save the idPlayer value 
-                        document.getElementById("d1").innerHTML = "<br>";                        
-                        idPlayer= data.idPlayer;
+                        document.getElementById("d1").innerHTML = "<br>"; 
+                        myPlayer = data;
                         
                         //Change the return message
                         document.getElementById("d4").innerHTML="<p class='blinky'>Created player</p>";
@@ -46,7 +46,7 @@ function newplayer(){
 
     if(created) 
         {
-            playgame();
+            playGame();
             //Message player displayed
             // response.innerHTML = output; 
             // document.getElementById("d2").innerHTML ="";
@@ -56,7 +56,7 @@ function newplayer(){
 
 //Play new game
 //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-function playgame()
+function playGame()
 {
     
     document.getElementById("d1").innerHTML="";
@@ -74,103 +74,101 @@ function playgame()
         tableString += "</tr>";
     }
     tableString += "</table>";
-    document.getElementById("d3").innerHTML = tableString;   
-      
-    alert("Aquí");
+    document.getElementById("d3").innerHTML = tableString; 
+
     //Charge the game menu
     var rows= document.getElementById("table1").rows[0].cells;
     //Exit            
     rows[0].innerHTML = 
-    "<a id='link' title='hello' href='./index.html'><img src='./images/exit_game.png' alt='exit' class='responsive'></a>";
+    "<a id='link' title='exit' href='./index.html'><img src='./images/exit_game.png' alt='exit' class='responsive'></a>";
     //Void
     rows[1].innerHTML = 
     "<img src='./images/happy.gif' alt='penguin' class='responsive'>";
     //Play game
     rows[2].innerHTML = 
-    "<a id='link' title='hello' href='javascript:startGame();'><img src='./images/play_again.png' alt='play' class='responsive'></a>";
+    "<a id='link' title='play' href='javascript:startGame();'><img src='./images/play_again.png' alt='play' class='responsive'></a>";
     }
     //+++++++++++++++++
-    function startGame(){
+    function startGame(){    
     
-    var response = document.getElementById("d4");
-    var name ="XD"; 
-    var dices = new Array(); 
-    var resultTxt= "Results: ";   
-    var winner = false;
-    var penguin = "XD";
+        var name ="XD"; 
+        var dices = new Array(); 
+        var resultTxt= "Results: ";   
+        var winner = false;
+        var penguin = "XD";
 
-    //Play the game
-    $.ajax
-    ({
-        url: urlServer+'players/'+idPlayer+"/games/",
-        type: 'POST',
-        async: false,
-        cache: false,
-        processData: false, 
-        contentType: "application/json; charset=utf-8",
-        dataType: "json", 
-        success: function(player)
-            {   
-                //Get the dice game result
-                var listGame = player.listGame; //Get the list of games
-                var numGames = listGame.length; //Get the number of games
-                numGames --;   //Get the current position game  
-                var lastGame = new Array();        
-                lastGame = listGame[numGames]; //Get the last game
-                winner = lastGame.isWinner;
-                //Get the result of the dices  
-                var listDiceResult = lastGame.listDiceResult; 
-                listDiceResult.forEach(
-                        function(results)
-                            {
-                                dices.push(results.result);                        
-                            }
-                    );                
-                //Get the player name
-                name = player.name;
-                //Change the return message
-                if(winner)
-                {
-                    //Wins
-                    // output= "<p>"+name+":<p class='blinky'>You win!</p>";
-                    output= "<p class='blinky'>You win!</p>";
-                    penguin="./images/happy.gif"
-                }
-                else
-                {
-                    //Lost
-                    // output= "<p>"+name+"<p class='blinkr'>You lost!</p>";
-                    output= "<p class='blinkr'>You lost!</p>";
-                    penguin="./images/cry.gif"
-                }
-                //Add the dice results
-                dices.forEach(function(result) 
+        //Play the game
+        $.ajax
+        ({
+            url: urlServer+'players/'+myPlayer.idPlayer+"/games/",
+            type: 'POST',
+            async: false,
+            cache: false,
+            processData: false, 
+            contentType: "application/json; charset=utf-8",
+            dataType: "json", 
+            success: function(player)
+                {   
+                    //Get the dice game result
+                    var listGame = player.listGame; //Get the list of games
+                    var numGames = listGame.length; //Get the number of games
+                    numGames --;   //Get the current position game  
+                    var lastGame = new Array();        
+                    lastGame = listGame[numGames]; //Get the last game
+                    winner = lastGame.isWinner;
+                    //Get the result of the dices  
+                    var listDiceResult = lastGame.listDiceResult; 
+                    listDiceResult.forEach(
+                            function(results)
+                                {
+                                    dices.push(results.result);                        
+                                }
+                        );                
+                    //Get the player name
+                    name = player.name;
+                    //Change the return message
+                    if(winner)
                     {
-                       resultTxt += result+" ";
-                    });
-                //Add the dices results
-                output+="<p>"+resultTxt+"</p>";
-                played=true;                        
-            },
-        error: function(xhr, ajaxOptions, thrownError)
-            {    
-                switch (xhr.status) 
-                    {
-                        case 409 : 
-                            output = "<p class='blinkr'>It was not possible to locate the player "+name+"</p>";
-                            break;
-                        case 404 :
-                            output = "<p class='blinkr'>Unable to play</p class='blinkr'>Error 404<p></p>" ;
-                            break;  
-                        default:   
-                            output = "<p class='blinkr'>Communications error:</p><p class='blinkr'>try later</p>";
+                        //Wins
+                        // output= "<p>"+name+":<p class='blinky'>You win!</p>";
+                        output= "<p class='blinky'>You win!</p>";
+                        penguin="./images/happy.gif"
                     }
-            }            
-    });   
-    //Change the output text
-    document.getElementById("d4").innerHTML = output; 
-    //Change the penguin image
-    var rows= document.getElementById("table1").rows[0].cells;
-    rows[1].innerHTML = "<img src='"+penguin+"' class='responsive'> ";
+                    else
+                    {
+                        //Lost
+                        // output= "<p>"+name+"<p class='blinkr'>You lost!</p>";
+                        output= "<p class='blinkr'>You lost!</p>";
+                        penguin="./images/cry.gif"
+                    }
+                    //Add the dice results
+                    dices.forEach(function(result) 
+                        {
+                        resultTxt += result+" ";
+                        });
+                    //Add the dices results
+                    output+="<p>"+resultTxt+"</p>";
+                    played=true;                        
+                },
+            error: function(xhr, ajaxOptions, thrownError)
+                {    
+                    switch (xhr.status) 
+                        {
+                            case 409 : 
+                                output = "<p class='blinkr'>It was not possible to locate the player "+name+"</p>";
+                                break;
+                            case 404 :
+                                output = "<p class='blinkr'>Unable to play</p class='blinkr'>Error 404<p></p>" ;
+                                break;  
+                            default:   
+                                output = "<p class='blinkr'>Communications error:</p><p class='blinkr'>try later</p>";
+                        }
+                }            
+        });   
+        //Change the output text
+        document.getElementById("d4").innerHTML = output; 
+        //Change the penguin image
+        var rows= document.getElementById("table1").rows[0].cells;
+        rows[1].innerHTML = "<img src='"+penguin+"' class='responsive'> ";
 }  
 
--- js/select_player.js
+++ js/select_player.js
@@ -96,7 +96,7 @@ function submenu(){
     document.getElementById("d2").innerHTML="<a href='javascript:renamePlayer();'>Rename player</a>";
     document.getElementById("d3").innerHTML="<br>"+"<a href='javascript:deletePlayer();'>Delete player</a>";
     document.getElementById("d4").innerHTML="<br>"+"<a href='javascript:resetGames();'>Reset games</a>";
-    document.getElementById("d5").innerHTML="<br>"+"<a href='#' onclick='#'>Play Game</a>"; 
+    document.getElementById("d5").innerHTML="<br>"+"<a href='javascript:playGame()'>Play Game</a>"; 
 }
 
 //Delete player
@@ -282,3 +282,127 @@ function getall(){
         document.getElementById("incoming").innerHTML = response
         }).catch(error => console.error(error))
 }
+
+//Play new game
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function playGame()
+{
+    var idPlayer= myPlayer.idPlayer;
+    
+    document.getElementById("d1").innerHTML="";
+    document.getElementById("d2").innerHTML=""; 
+    document.getElementById("d4").innerHTML="";
+    document.getElementById("d5").innerHTML="<p>It's play time!</p>"; 
+    //Create the table   
+
+    var tableString = "<table id='table1' style='width: 100%'>";
+    for (row = 1; row < 2; row += 1) 
+    {
+        tableString += "<tr>";
+            for (col = 1; col < 4; col += 1) 
+            {
+                tableString += "<td class='thirtythree'> </td>";
+            }
+        tableString += "</tr>";
+    }
+    tableString += "</table>";
+    document.getElementById("d3").innerHTML = tableString; 
+      
+    //Charge the game menu
+    var rows= document.getElementById("table1").rows[0].cells;
+    //Exit            
+    rows[0].innerHTML = 
+    "<a id='link' title='exit' href='./index.html'><img src='./images/exit_game.png' alt='exit' class='responsive'></a>";
+    //Void
+    rows[1].innerHTML = 
+    "<img src='./images/happy.gif' alt='penguin' class='responsive'>";
+    //Play game
+    rows[2].innerHTML = 
+    "<a id='link' title='play' href='javascript:startGame();'><img src='./images/play_again.png' alt='play' class='responsive'></a>";  
+    }
+
+
+    //+++++++++++++++++
+function startGame(){
+    
+        var name ="XD"; 
+        var dices = new Array(); 
+        var resultTxt= "Results: ";   
+        var winner = false;
+        var penguin = "XD";
+        
+
+        //Play the game
+        $.ajax
+        ({
+            url: urlServer+'players/'+myPlayer.idPlayer+"/games/",
+            type: 'POST',
+            async: false,
+            cache: false,
+            processData: false, 
+            contentType: "application/json; charset=utf-8",
+            dataType: "json", 
+            success: function(player)
+                {   
+                    //Get the dice game result
+                    var listGame = player.listGame; //Get the list of games
+                    var numGames = listGame.length; //Get the number of games
+                    numGames --;   //Get the current position game  
+                    var lastGame = new Array();        
+                    lastGame = listGame[numGames]; //Get the last game
+                    winner = lastGame.isWinner;
+                    //Get the result of the dices  
+                    var listDiceResult = lastGame.listDiceResult; 
+                    listDiceResult.forEach(
+                            function(results)
+                                {
+                                    dices.push(results.result);                        
+                                }
+                        );                
+                    //Get the player name
+                    name = player.name;
+                    //Change the return message
+                    if(winner)
+                    {
+                        //Wins
+                        // output= "<p>"+name+":<p class='blinky'>You win!</p>";
+                        output= "<p class='blinky'>You win!</p>";
+                        penguin="./images/happy.gif"
+                    }
+                    else
+                    {
+                        //Lost
+                        // output= "<p>"+name+"<p class='blinkr'>You lost!</p>";
+                        output= "<p class='blinkr'>You lost!</p>";
+                        penguin="./images/cry.gif"
+                    }
+                    //Add the dice results
+                    dices.forEach(function(result) 
+                        {
+                        resultTxt += result+" ";
+                        });
+                    //Add the dices results
+                    output+="<p>"+resultTxt+"</p>";
+                    played=true;                        
+                },
+            error: function(xhr, ajaxOptions, thrownError)
+                {    
+                    switch (xhr.status) 
+                        {
+                            case 409 : 
+                                output = "<p class='blinkr'>It was not possible to locate the player "+name+"</p>";
+                                break;
+                            case 404 :
+                                output = "<p class='blinkr'>Unable to play</p class='blinkr'>Error 404<p></p>" ;
+                                break;  
+                            default:   
+                                output = "<p class='blinkr'>Communications error:</p><p class='blinkr'>try later</p>";
+                        }
+                }            
+        });   
+        //Change the output text
+        document.getElementById("d4").innerHTML = output; 
+        //Change the penguin image
+        var rows= document.getElementById("table1").rows[0].cells;
+        rows[1].innerHTML = "<img src='"+penguin+"' class='responsive'> ";
+}  
